76bc817424b4a995f2bdc05abced4ae5fe429987
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-07 19:13:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function additionByOne(num){
+    return num +1;
+}
+console.log(additionByOne(0));
+console.log(additionByOne(9));
+console.log(additionByOne(-3));
